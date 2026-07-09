f5dc4e2be2be6c759363bfccf5bae613dd102ca1
satoshinotdead <dev@criptonautas.co>
2026-07-09 15:18:03 -0500

    edit

--- javascripts/closed-sidebar-by-default/initializers/closed-sidebar-by-default-init.js
+++ javascripts/closed-sidebar-by-default/initializers/closed-sidebar-by-default-init.js
@@ -1,4 +1,7 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
+import { addObserver } from "@ember/object/observers";
+
+const SIDEBAR_MOBILE_KEY = "discourse_narative_Sidebar_showSidebar";
 
 export default {
   name: "closed-sidebar-by-default",
@@ -6,7 +9,32 @@ export default {
   initialize() {
     withPluginApi("0.8", (api) => {
       const applicationController = api.container.lookup("controller:application");
-      applicationController.set("showSidebar", false);
+      const site = api.container.lookup("site:main");
+
+      const applySidebarState = () => {
+        const isMobile = site.mobileView;
+
+        if (isMobile) {
+          const storedState = localStorage.getItem(SIDEBAR_MOBILE_KEY);
+          if (storedState === null) {
+            applicationController.set("showSidebar", false);
+            localStorage.setItem(SIDEBAR_MOBILE_KEY, "false");
+          } else {
+            applicationController.set("showSidebar", storedState === "true");
+          }
+        } else {
+          const storedState = localStorage.getItem(SIDEBAR_MOBILE_KEY);
+          if (storedState === "false") {
+            applicationController.set("showSidebar", false);
+          }
+        }
+      };
+
+      applySidebarState();
+
+      addObserver(site, "mobileView", () => {
+        applySidebarState();
+      });
     });
   },
 };
